610465d6a0d93468d7ba7be0beb9ebf5f272baaf
minzhang190 <minzhang190@gmail.com>
2020-05-09 07:43:54 +0000

    gaid.js

--- assets/js/message.js
+++ assets/js/message.js
@@ -4,22 +4,7 @@
         return;
     }
 
-    var count = 50;
-    var interval = setInterval(() => {
-        if (count-- > 0 && (!window.ga || !window.ga.getAll)) {
-            console.log('Waiting for window.ga to be available...');
-            return;
-        }
-
-        clearInterval(interval);
-
-        var trackerId = null;
-        if (window.ga && window.ga.getAll) {
-            trackerId = ga.getAll().map(tracker => tracker.get('clientId')).filter(x => x).pop();
-        }
-        if (!trackerId) {
-            return;
-        }
+    useGAID(function(trackerId) {
 
         fetch('https://hawken-c9ef9.web.app/message/' + trackerId + '.html').then(function(response) {
             if (!response.ok) {
@@ -53,5 +38,6 @@
         };
 
         documents(0);
-    }, 100);
+
+    });
 })();
